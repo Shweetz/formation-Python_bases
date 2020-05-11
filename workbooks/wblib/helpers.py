c92bbffbd721666caddfdc74bcf8648496871d64
Romain Lambert <rmnlmbrt@gmail.com>
2020-05-11 18:16:24 +0200

    Formation Python SII 5 jours

--- workbooks/wblib/helpers.py
+++ workbooks/wblib/helpers.py
@@ -12,3 +12,25 @@ Notez également que ce commentaire est une documentation du module. Il s'agit
 d'une docstring du module.
 """
 
+
+def duration_for(how_many: int, unit_duration=7):
+    """
+    Calcul d'une durée totale en fonction d'une durée unitaire
+
+    :param how_many: Le nombre d'unités, doit être positif ou nul
+    :param unit_duration:
+    :return:
+    :raise ValueError: Si les valeur sont négatives
+    """
+    how_many = int(how_many)
+    unit_duration = int(unit_duration)
+
+    if how_many < 0:
+        raise ValueError("Quantities should be positive")
+    if unit_duration < 0:
+        raise ValueError("Duration should be positive")
+
+    if how_many >= 0 and unit_duration >= 0:
+        return how_many * unit_duration
+
+print(duration_for("5"))
